2c4356cfc24fda88c0fe6d4b874cd52276a83384
megz <magedelshafey98@gmail.com>
2024-10-16 00:31:12 +0300

    configure react query

--- src/index.js
+++ src/index.js
@@ -7,10 +7,21 @@ import AppRouter from "./routes/AppRouter";
 // language
 import "./languages/i18n";
 import AppInit from "./components/common/app/AppInit";
+// react query
+import { QueryClientProvider, QueryClient } from "react-query";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+      cacheTime: 0,
+      refetchOnMount: true,
+    },
+  },
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
+  <QueryClientProvider client={queryClient}>
     <AppInit />
     <AppRouter />
-  </>
+  </QueryClientProvider>
 );
