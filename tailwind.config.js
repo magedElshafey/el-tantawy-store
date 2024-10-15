e62d24a6ec42b12f42756f6ad1a7f0450745e420
megz <magedelshafey98@gmail.com>
2024-10-16 00:49:19 +0300

    add global style and configure tailwind css color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        redColor: "#de0712",
+        blackColor: "#000",
+        yellowColor: "#f3de6d",
+        grayColor: "#fafafa",
+        textColor: "#555",
+        darkColor: "#191c1f",
+        graySection: "#fafafa",
+        sectionColor: "#fcc8c74d",
+      },
+    },
   },
   plugins: [],
 };
