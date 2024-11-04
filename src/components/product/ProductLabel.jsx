3cd506e28b5c14cc3296d06ec1b621a8e2e88eb6
magedElshafey <magedelshafey98@gmail.com>
2024-11-04 11:55:16 +0200

    updates

---
+++ src/components/product/ProductLabel.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import createSlug from "../../utils/createSlug";
+import ProductSlider from "../common/products/ProductSlider";
+const ProductLabel = ({ item, products }) => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <div className="w-full py-5 px-8 md:px-16 flex items-center justify-between text-nowrap bg-redColor text-white rounded-md mb-4 md:mb-6 lg:mb-8 xl:mb-12">
+        <p className="font-bold text-base md:text-md lg:text-lg xl:text-xl">
+          {item?.name}
+        </p>
+        <Link
+          to={`/category/${createSlug(item?.name)}`}
+          state={{
+            categoryId: item?.id,
+            categoryName: item?.name,
+          }}
+          className=" underline"
+        >
+          {t("browse more")}
+        </Link>
+      </div>
+      <div className="container">
+        <ProductSlider data={products} />
+      </div>
+    </>
+  );
+};
+
+export default ProductLabel;
+/**
+ *   <div className="w-full py-5 flex items-center bg-redColor">
+      <div className="container flex gap-4 justify-between  text-white text-nowrap items-center ">
+      
+     
+      </div>
+    </div>
+ */
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,13 +13,14 @@ import offerBanner from "../assets/بانر-العروض.png";
 import buyNow from "../assets/اشتري-دلوقتي-وادفع-بعدين.png";
 import waitUs from "../assets/استنونا-كل-يوم.png";
 import haram from "../assets/هرم-الطنطاوي-الرابع.png";
-import aboElBanat from "../assets/ابو-البنات.png";
 import atgawez from "../assets/عايزة-اتجوز.png";
+import ProductLabel from "../components/product/ProductLabel";
 const Home = () => {
   const { data } = useGlobalContext();
   const { t } = useTranslation();
   const navigate = useNavigate();
-  const handleShopPageNavigate = () => navigate("/shop");
+  const handleOffersPageNavigate = () => navigate("/offers");
+  const handlePrideOffersNavigate = () => navigate("/pride-offers");
   return (
     <>
       <CategoriesBannersSlider data={data?.banners} />
@@ -29,44 +30,6 @@ const Home = () => {
           {data?.features?.map((item, index) => (
             <FeatuersCard key={index} data={item} />
           ))}
-        </div>
-      </div>
-      {/*most popular products*/}
-      <div className="container">
-        <div className="my-4 md:my-6 lg:my-8 xl:my-12">
-          <div className="flex items-center justify-between gap-3 flex-wrap text-nowrap mb-3">
-            <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl">
-              {t("Most popular products")}
-            </p>
-            <div className="w-[200px]">
-              <MainBtn
-                text="show more products"
-                action={handleShopPageNavigate}
-              />
-            </div>
-          </div>
-          <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">
-            {data?.maxOrderProducts?.slice(0, 12)?.map((item, index) => (
-              <ProductCard data={item} key={index} />
-            ))}
-          </div>
-        </div>
-      </div>
-      {/*haram*/}
-      <div className="mb-4 md:mb-6 lg:mb-8 xl:mb-12">
-        <SingleBanner alt="el-tantawy" src={haram} />
-      </div>
-      {/**main category slider*/}
-      <div className="w-full  bg-graySection  py-5 flex items-center my-4 md:my-6 lg:my-8 xl:my-12">
-        <CategorySlider data={data?.categories} />
-      </div>
-      {/**customized product */}
-      <div className="container">
-        <div className="my-4 md:my-6 lg:my-8 xl:my-12 ">
-          <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-            {t("customized")}
-          </p>
-          <ProductSlider data={data?.productsForYou} />
         </div>
       </div>
       {/**offer banner*/}
@@ -80,31 +43,40 @@ const Home = () => {
           height="h-[70px] md:h-[100px] lg:h-[200px]"
         />
       </Link>
-      {/** best seller */}
-      <div className="container my-4 md:my-6 lg:my-8 xl:my-12 ">
-        <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-          {t("best")}
-        </p>
+      {/*offer products*/}
+      <div className="container my-4 md:my-6 lg:my-8 xl:my-12">
+        <div className="w-full flex justify-start lg:justify-end">
+          <div className="w-[200px] mb-4 md:mb-6">
+            <MainBtn
+              text="show more offers"
+              action={handleOffersPageNavigate}
+            />
+          </div>
+        </div>
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">
-          {data?.shippingProducts?.slice(0, 12)?.map((item, index) => (
-            <ProductCard key={index} data={item} />
+          {data?.maxOrderProducts?.slice(0, 12)?.map((item, index) => (
+            <ProductCard data={item} key={index} />
           ))}
         </div>
       </div>
-      {/** best seller */}
-      <div className="container my-4 md:my-6 lg:my-8 xl:my-12 ">
-        <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-          {t("best")}
-        </p>
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">
-          {data?.shippingProducts?.slice(0, 12)?.map((item, index) => (
-            <ProductCard key={index} data={item} />
-          ))}
-        </div>
-      </div>
-      {/*aboElBanat*/}
+      {/*haram*/}
       <div className="mb-4 md:mb-6 lg:mb-8 xl:mb-12">
-        <SingleBanner alt="el-tantawy" src={aboElBanat} />
+        <SingleBanner alt="el-tantawy" src={haram} />
+      </div>
+
+      {/**customized product */}
+      <div className="container">
+        <div className="my-4 md:my-6 lg:my-8 xl:my-12 ">
+          <ProductSlider data={data?.productsForYou} />
+        </div>
+      </div>
+
+      {/**main category slider*/}
+      <div className="my-4 md:my-6 lg:my-8 xl:my-12 container text-md md:text-lg lg:text-xl xl:text-2xl text-center font-bold text-redColor">
+        {t("browse by category")}
+      </div>
+      <div className="w-full my-4 md:my-6 lg:my-8 xl:my-12  bg-graySection  py-5 flex items-center ">
+        <CategorySlider data={data?.categories} />
       </div>
       {/**customized product */}
       <div className="container">
@@ -113,152 +85,58 @@ const Home = () => {
             {t("customized")}
           </p>
           <ProductSlider data={data?.productsForYou} />
-        </div>
-      </div>
-      {/** best seller */}
-      <div className="container my-4 md:my-6 lg:my-8 xl:my-12 ">
-        <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-          {t("best")}
-        </p>
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">
-          {data?.shippingProducts?.slice(0, 12)?.map((item, index) => (
-            <ProductCard key={index} data={item} />
-          ))}
         </div>
       </div>
       {/**installment banner*/}
       <div className=" overflow-x-hidden my-4 md:my-6 lg:my-8 xl:my-12 ">
         <SingleBanner alt="installment" src={buyNow} />
       </div>
-      {/**customized product */}
-      <div className="container">
-        <div className="my-4 md:my-6 lg:my-8 xl:my-12 ">
-          <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-            {t("customized")}
-          </p>
-          <ProductSlider data={data?.productsForYou} />
-        </div>
-      </div>
       {/** best seller */}
       <div className="container my-4 md:my-6 lg:my-8 xl:my-12 ">
-        <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-          {t("best")}
-        </p>
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">
           {data?.shippingProducts?.slice(0, 12)?.map((item, index) => (
             <ProductCard key={index} data={item} />
           ))}
         </div>
       </div>
-      {/**customized product */}
-      <div className="container">
-        <div className="my-4 md:my-6 lg:my-8 xl:my-12 ">
-          <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-            {t("customized")}
-          </p>
-          <ProductSlider data={data?.productsForYou} />
-        </div>
-      </div>
-      {/** best seller */}
-      <div className="container my-4 md:my-6 lg:my-8 xl:my-12 ">
-        <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-          {t("best")}
-        </p>
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">
-          {data?.shippingProducts?.slice(0, 12)?.map((item, index) => (
-            <ProductCard key={index} data={item} />
-          ))}
-        </div>
-      </div>
-      {/**offer banner*/}
+      {/**pride banner*/}
       <Link
         to="pride-offers"
-        className="w-screen overflow-x-hidden my-4 md:my-6 lg:my-8 xl:my-12"
+        className="w-screen block overflow-x-hidden my-4 md:my-6 lg:my-8 xl:my-12"
       >
         <SingleBanner alt="atgawez" src={atgawez} />
       </Link>
-      {/**customized product */}
-      <div className="container">
-        <div className="my-4 md:my-6 lg:my-8 xl:my-12 ">
-          <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-            {t("customized")}
-          </p>
-          <ProductSlider data={data?.productsForYou} />
+      <div className="container my-4 md:my-6 lg:my-8 xl:my-12">
+        <div className="w-full flex justify-start lg:justify-end">
+          <div className="w-[200px] mb-4 md:mb-6">
+            <MainBtn
+              text="show more offers"
+              action={handlePrideOffersNavigate}
+            />
+          </div>
         </div>
-      </div>
-      {/** best seller */}
-      <div className="container my-4 md:my-6 lg:my-8 xl:my-12 ">
-        <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-          {t("best")}
-        </p>
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">
-          {data?.shippingProducts?.slice(0, 12)?.map((item, index) => (
-            <ProductCard key={index} data={item} />
+          {data?.maxOrderProducts?.slice(0, 12)?.map((item, index) => (
+            <ProductCard data={item} key={index} />
           ))}
         </div>
       </div>
-      {/**customized product */}
-      <div className="container">
-        <div className="my-4 md:my-6 lg:my-8 xl:my-12 ">
-          <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-            {t("customized")}
-          </p>
-          <ProductSlider data={data?.productsForYou} />
-        </div>
-      </div>
-      {/** best seller */}
-      <div className="container my-4 md:my-6 lg:my-8 xl:my-12 ">
-        <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-          {t("best")}
-        </p>
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">
-          {data?.shippingProducts?.slice(0, 12)?.map((item, index) => (
-            <ProductCard key={index} data={item} />
-          ))}
-        </div>
-      </div>
+      {/**wait us*/}
       <div className="w-screen overflow-x-hidden my-4 md:my-6 lg:my-8 xl:my-12">
         <SingleBanner alt="wait" src={waitUs} />
       </div>
       {/**customized product */}
       <div className="container">
         <div className="my-4 md:my-6 lg:my-8 xl:my-12 ">
-          <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-            {t("customized")}
-          </p>
           <ProductSlider data={data?.productsForYou} />
         </div>
       </div>
-      {/** best seller */}
-      <div className="container my-4 md:my-6 lg:my-8 xl:my-12 ">
-        <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-          {t("best")}
-        </p>
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">
-          {data?.shippingProducts?.slice(0, 12)?.map((item, index) => (
-            <ProductCard key={index} data={item} />
-          ))}
-        </div>
-      </div>
-      {/**customized product */}
-      <div className="container">
-        <div className="my-4 md:my-6 lg:my-8 xl:my-12 ">
-          <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-            {t("customized")}
-          </p>
-          <ProductSlider data={data?.productsForYou} />
-        </div>
-      </div>
-      {/** best seller */}
-      <div className="container my-4 md:my-6 lg:my-8 xl:my-12 ">
-        <p className="font-bold text-redColor text-md md:text-lg lg:text-xl xl:text-2xl mb-5">
-          {t("best")}
-        </p>
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">
-          {data?.shippingProducts?.slice(0, 12)?.map((item, index) => (
-            <ProductCard key={index} data={item} />
-          ))}
-        </div>
+      <div className="my-4 md:my-6 lg:my-8 xl:my-12">
+        {data?.categories?.map((item, index) => (
+          <div key={index} className="mb-4 md:mb-6 lg:mb-8 xl:mb-12">
+            <ProductLabel item={item} products={data?.productsForYou} />
+          </div>
+        ))}
       </div>
     </>
   );
